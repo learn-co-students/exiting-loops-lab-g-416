24f7c98dba5b7521b0841862aa541ea78ff4f7f1
Brian <briandsalemi@gmail.com>
2019-11-09 12:00:30 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function breakOut(array, changeValue, stopValue){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] == stopValue){
+            break;
+        } else {
+            array[i] = changeValue
+        }
+    }
+}
+
+function keepGoing(array, changeValue, skipValue){
+
+}
+
+function findBy(array, findFn){
+
+}
