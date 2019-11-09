3f08ca5639844c83224f188c7a6332a3b1d4095f
Brian <briandsalemi@gmail.com>
2019-11-09 12:31:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function breakOut(array, changeValue, stopValue){
     for(let i = 0; i < array.length; i++){
-        if(array[i] == stopValue){
+        if(array[i] === stopValue){
             break;
-        } else {
-            array[i] = changeValue
         }
+            array[i] = changeValue
     }
+    return array 
 }
 
 function keepGoing(array, changeValue, skipValue){
-
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === skipValue){
+            continue;
+        }
+        array[i] = changeValue
+    }
+    return array 
 }
 
 function findBy(array, findFn){
-
+    for(let i = 0; i < array.length; i++){
+        if(findFn(array[i])){
+            return array[i]
+        }
+    }
+    return null 
 }
